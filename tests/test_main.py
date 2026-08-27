13460d25c8507b59b7e51152492eedb190d132b7
janacibron <acibronjan@gmail>
2026-08-28 02:11:21 +0800

    Fix: import order for ruff

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 import pytest
-from httpx import AsyncClient, ASGITransport
+from httpx import ASGITransport, AsyncClient
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
